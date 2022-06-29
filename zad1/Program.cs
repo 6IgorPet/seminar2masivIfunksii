8cda5eeda1036c466bd59f41f3d7e3728e2850de
igor <desant350@gmail.com>
2022-06-29 22:45:33 +0300

    zad1

---
+++ zad1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+System.Console.WriteLine("трёхзначное число инажмите enter");
+while(true)
+{
+    string input = Console.ReadLine();
+    if(!input.Equals("exit"))
+    Console.WriteLine("{0}->{1}",input, input[1]);
+     else
+     break;
+}
+
+
+
